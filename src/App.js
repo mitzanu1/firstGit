b8f1ac052e932a7f185671b8603d20a647db594f
mitzanu <mitzanu@hotmail.com>
2020-11-29 18:42:35 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import Interface from './Routes/Interface/Interface';
 import React from 'react'
 import Circle from './Routes/Circle/Circle';
-import Controler from './Routes/Controler/Controler';
 
 function App() {
   return <>
     <Interface/>
-    <Controler/>
     <Circle/>
   </>
 }
